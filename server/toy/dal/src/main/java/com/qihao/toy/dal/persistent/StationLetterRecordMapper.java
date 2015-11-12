08df5b1a19b108ba3eff90f9a3630081d00e3652
guchangxin <guchangxin@1c380969-13fc-4aa0-adf8-6006e7eac7e8>
2015-11-12 04:24:46 +0000

    ddddd

---
+++ server/toy/dal/src/main/java/com/qihao/toy/dal/persistent/StationLetterRecordMapper.java
@@ -0,0 +1,9 @@
+package com.qihao.toy.dal.persistent;
+
+import com.qihao.toy.dal.domain.StationLetterRecordDO;
+
+
+
+public interface StationLetterRecordMapper extends CRUDMapper<StationLetterRecordDO> {
+
+}
